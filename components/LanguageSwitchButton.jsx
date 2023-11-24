3d5ee1b1cfeac912826ae92b0d42f50a86cfeb8a
JoMar <contact@jomar.fr>
2023-11-24 20:31:04 +0100

    Update LanguageSwitchButton.jsx

--- components/LanguageSwitchButton.jsx
+++ components/LanguageSwitchButton.jsx
@@ -86,7 +86,8 @@ export default function LanguageSwitchButton() {
 		<LanguageWrapper>
 			<Image src="/img/frfr.png" alt="Bandeira do Brasil" width={28} height={28} objectFit="cover" />
 			<SwitchButton>
-				<label htmlFor="lang-switch" className="switch">
+				<label className="switch">
+					Lang Switch
 					<input type="checkbox" checked={checked} onClick={handleCheckSwitch} readOnly data-splitbee-event="language-change" />
 					<span className="slider round"></span>
 				</label>
